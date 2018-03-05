f9ddb307e649459496ce4a4ddb554c82bbe0908f
Narahari, Sainath <sainath.narahari@ge.com>
2018-03-05 12:58:13 -0600

    Angular Forms with Input Field Validations and Errors with ngModel

--- src/app/components/contact-form/contact-form.component.ts
+++ src/app/components/contact-form/contact-form.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-form.component.css']
 })
 export class ContactFormComponent implements OnInit {
+
   log(x) {
     console.log(x);
   }
